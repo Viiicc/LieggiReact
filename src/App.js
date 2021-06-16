fc858d7a9c952b1eaacfd7cd79ea16eb461a1581
Viiicc <victor.lieggi@gmail.com>
2021-06-16 01:13:13 -0300

    se agrega contador

--- src/App.js
+++ src/App.js
@@ -1,19 +1,45 @@
 import React, {Component} from 'react';
-import './App.css';
+import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'semantic-ui-react';
 import NavBar from './components/NavBar';
 import Catalogo from './components/catalogo';
 
 
 
 
+
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      count: 0,
+    };
+
+  }
+    handleCounterUp = () => {
+      this.setState({count: this.state.count + 1})
+    }
+    handleCounterDown = () => {
+      this.setState({count: this.state.count - 1})
+    }
+  
   render () {
     return (
       <div>
         <NavBar/>
         <Catalogo subtitle={'Paciencia, ya va tomando forma'} />
+        <div className="counter-section">
+          <h3>Contador:</h3>
+          <p>{this.state.count}</p>
+          <div className="btn-section">
+            <Button onClick={this.handleCounterUp} primary>+</Button>
+            <Button onClick={this.handleCounterDown} positive>-</Button>
+          </div>
+        </div>
       </div>
+      
   
     );
   }
